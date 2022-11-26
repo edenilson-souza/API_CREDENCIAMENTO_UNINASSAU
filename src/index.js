49cd2a71a610ca66b78e8fb5b4854794ac627159
Edenilson Souza <edenilson.sza@gmail.com>
2022-11-26 14:17:30 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -27,21 +27,23 @@ async function start() {
     app.use(bodyParser.urlencoded({limit:'50mb', extended: false }));
     app.use(bodyParser.json({limit: 10 * 1024 * 1024}));
     app.use(bodyParser.raw({limit: 10 * 1024 * 1024}));
-    app.use(cors({ origin: process.env.CORS_ORIGIN || '*' }));
-    app.use(helmet());
+
+  /*   app.use(cors({ origin: process.env.CORS_ORIGIN || '*' })); */
+
+  /*   app.use(helmet()); */
     app.use(express.json());
     
     //upload de arquivos
-    app.use(fileupload({
+    /* app.use(fileupload({
         useTempFiles : true,
         tempFileDir : '/tmp/',
         limits: { fileSize: 50 * 1024 * 1024 },
-    }));
+    })); */
 
     //SAVE LOGGING TO FILE
     //var accessLogStream = rfs.createStream("access.log", {size: "10M", interval: "1d", compress: "gzip", path: path.join(__dirname, '../log')});
-    morgan.token('token', function getId (req) {return req.headers['access-token'];});
-    app.use(morgan(':remote-addr - - [:date[clf]] ":method :url HTTP/:http-version" Status=:status Size=:res[content-length] Referrer=":referrer" User-Agent":user-agent" Response-Time=:response-time Token=":token"', {skip: function(req, res){return res.statusCode < 400 }}));
+  /*   morgan.token('token', function getId (req) {return req.headers['access-token'];});
+    app.use(morgan(':remote-addr - - [:date[clf]] ":method :url HTTP/:http-version" Status=:status Size=:res[content-length] Referrer=":referrer" User-Agent":user-agent" Response-Time=:response-time Token=":token"', {skip: function(req, res){return res.statusCode < 400 }})); */
     //app.use(morgan(':remote-addr - - [:date[clf]] ":method :url HTTP/:http-version" Status=:status Size=:res[content-length] Referrer=":referrer" User-Agent":user-agent" Response-Time=:response-time Token=":token"', {stream: accessLogStream}));
 
 
@@ -66,12 +68,12 @@ async function start() {
 
 
     //REMOVE INVALID TOKENS
-    timexe ("* * w1-7 / 6" ,  function ( ) {  
+   /*  timexe ("* * w1-7 / 6" ,  function ( ) {  
         const validade = new Date(Date.now() - (1000* 60 * 20)); //Deletar todos os tokens que tiverem mais de 20 minutos que foram inseridos
         knex('tokens').where('created_at', '<', validade).del();
         // * * * 0-23 / 1                   //A CADA 1 minuto
         //* * w1-7 / 6                      //A CADA 6 horas
-    });
+    }); */
 
 }
 
